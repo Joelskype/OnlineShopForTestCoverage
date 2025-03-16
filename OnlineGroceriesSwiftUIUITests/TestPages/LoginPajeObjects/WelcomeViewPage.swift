181dbe82117f4aece308103c5bd514157803b43a
Alexey <ryagev-alexey@yandex.ru>
2025-03-16 20:38:13 +0700

    Add folders for tests and pages. Setup targets on pgoj and on frameworks. Write one test for check

---
+++ OnlineGroceriesSwiftUIUITests/TestPages/LoginPajeObjects/WelcomeViewPage.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  OnlineShopForTestCoverageApp
+//
+//  Created by Алексей Ряжев on 3/16/25.
+//
+
+import XCTest
+@testable import OnlineShopForTestCoverage
+import SDWebImage
+
+final class WelcomeViewPageObject {
+    
+    private let app: XCUIApplication
+    
+    init(app: XCUIApplication) {
+        self.app = app
+    }
+}
+
